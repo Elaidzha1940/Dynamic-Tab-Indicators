46538364cd48b03030f39864a24e0981a0f8315f
Elaidzha1940 <eli080293@gmail.com>
2023-05-07 10:55:33 +0700

    added a new File / ScrollOffset

--- Dynamic Tab Indicators/Dynamic Tab Indicators/Helpers/ScrollOffset.swift
+++ Dynamic Tab Indicators/Dynamic Tab Indicators/Helpers/ScrollOffset.swift
@@ -11,6 +11,23 @@ extension View {
     @ViewBuilder
     func offsetX(completion: @escaping (CGRect) -> ()) -> some View {
         self
+            .overlay {
+                GeometryReader { proxy in
+                    let rect = proxy.frame(in: .global)
+                    
+                    Color.clear
+                        .preference(key: OffsetKey.self, value: rect)
+                }
+            }
+        
+    }
+}
+
+struct OffsetKey: PreferenceKey {
+    static var defaultValue: CGRect = .zero
+    
+    static func reduce(value: inout CGRect, nextValue: () -> CGRect) {
+        value = nextValue()
         
     }
 }
