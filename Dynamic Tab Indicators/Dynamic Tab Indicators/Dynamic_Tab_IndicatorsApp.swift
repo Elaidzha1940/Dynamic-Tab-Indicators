d14d4efc53bb6e0bff51c48fc5de665b30be6597
Elaidzha1940 <eli080293@gmail.com>
2023-05-07 09:51:22 +0700

    Dynamic Tab Indicators

---
+++ Dynamic Tab Indicators/Dynamic Tab Indicators/Dynamic_Tab_IndicatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dynamic_Tab_IndicatorsApp.swift
+//  Dynamic Tab Indicators
+//
+//  Created by Elaidzha Shchukin on 07.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct Dynamic_Tab_IndicatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
